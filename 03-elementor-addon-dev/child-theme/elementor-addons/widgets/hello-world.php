a2384b7b42a4aba0acae0e01b358a5d8d93e7bbe
mhasanmeet <mhasan.meet@gmail.com>
2023-06-25 20:45:56 +0600

    my-first-elementor-addon

--- 03-elementor-addon-dev/child-theme/elementor-addons/widgets/hello-world.php
+++ 03-elementor-addon-dev/child-theme/elementor-addons/widgets/hello-world.php
@@ -6,11 +6,11 @@ class Mhasan_Elementor_Addon_Hello_World extends \Elementor\Widget_Base {
 	}
 
 	public function get_title() {
-		return esc_html__( 'Hello World', 'elementor-addon' );
+		return esc_html__( '@mhasan', 'elementor-addon' );
 	}
 
 	public function get_icon() {
-		return 'eicon-code';
+		return 'eicon-elementor';
 	}
 
 	public function get_categories() {
@@ -18,13 +18,38 @@ class Mhasan_Elementor_Addon_Hello_World extends \Elementor\Widget_Base {
 	}
 
 	public function get_keywords() {
-		return [ 'hello', 'world' ];
+		return [ 'mhasan', 'hello', 'world' ];
+	}
+
+	protected function register_controls(){
+		// Elementor addon tab title
+		$this->start_controls_section(
+			'section_title',
+			[
+				"label" => esc_html__("Title @mhasan", "elementor-addons"),
+				"tab" => \Elementor\Controls_Manager::TAB_CONTENT
+			]
+		);
+
+		$this->add_control(
+			'title',
+			[
+				"label" => esc_html__("Write your content @mhasan", "elementor-addons"),
+				"type" => \Elementor\Controls_Manager::TEXTAREA,
+				"default" => esc_html("Write your content here @mhasan", "elementor-addon")
+			]
+		);
+
+		$this->end_controls_section();
 	}
 
 	protected function render() {
+
+		$settings = $this->get_settings_for_display();
+
 		?>
 
-			<p> Hello World </p>
+			<p> <?php echo $settings['title']; ?> </p>
 
 		<?php
 	}
